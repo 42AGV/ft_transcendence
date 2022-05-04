a3f9938a132632f763a7e5f6db6f9c37771b1cff
gbudau <54190400+gbudau@users.noreply.github.com>
2022-05-04 15:22:19 +0200

    Add config validation with class-validator

--- transcendence-app/src/app.module.ts
+++ transcendence-app/src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
 import { OAuth42Module } from './oauth42/oauth42.module';
+import { validate } from './config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      validationSchema: Joi.object({
-        FORTYTWO_APP_ID: Joi.string().required(),
-        FORTYTWO_APP_SECRET: Joi.string().required(),
-        FORTYTWO_APP_CALLBACK_URL: Joi.string().uri().required(),
-      }),
+      validate,
     }),
     OAuth42Module,
   ],
