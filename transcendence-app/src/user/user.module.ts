440fe1a93791c0837064cd0738333560c5ffc424
gbudau <54190400+gbudau@users.noreply.github.com>
2022-05-08 21:24:28 +0200

    Update user.module.ts
    Remove the HttpModule imports

--- transcendence-app/src/user/user.module.ts
+++ transcendence-app/src/user/user.module.ts
@@ -1,9 +1,7 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [HttpModule],
   providers: [UserService],
   exports: [UserService],
 })
