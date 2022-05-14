00ba3b9b8059d55f3c01dcf941383d62a1818cb2
gbudau <54190400+gbudau@users.noreply.github.com>
2022-05-14 15:04:55 +0200

    Update OAuth42Guard
    Catch exceptions thrown by PassportJs
    Throw BadGatewayException

--- transcendence-app/src/oauth42/oauth42.guard.ts
+++ transcendence-app/src/oauth42/oauth42.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class OAuth42Guard extends AuthGuard('oauth42') {}
+export class OAuth42Guard extends AuthGuard('oauth42') {
+  handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw new BadGatewayException();
+    }
+    return user;
+  }
+}
