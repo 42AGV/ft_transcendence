c8bfb5ea5f782eac7027d4a1876774a49870699c
gbudau <54190400+gbudau@users.noreply.github.com>
2022-05-14 15:02:32 +0200

    Add swagger

--- transcendence-app/src/oauth42/oauth42.controller.ts
+++ transcendence-app/src/oauth42/oauth42.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import { ApiBadGatewayResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from '../user/decorators/user.decorator';
 import { UserDto } from '../user/dto/user.dto';
 import { OAuth42Guard } from './oauth42.guard';
 
+@ApiTags('oauth42')
 @Controller('oauth42')
 export class OAuth42Controller {
   @Get()
+  @ApiResponse({ status: HttpStatus.FOUND })
+  @ApiBadGatewayResponse()
   @UseGuards(OAuth42Guard)
   async oauth42Login(@User() user: UserDto) {
     return user;
