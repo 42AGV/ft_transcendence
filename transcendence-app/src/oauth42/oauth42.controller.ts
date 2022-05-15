e228d730b3e962a04d5446cadd0c5fcb406833c8
gbudau <54190400+gbudau@users.noreply.github.com>
2022-05-15 19:30:09 +0200

    Update OAuth42Controller
    Use the @ApiFoundResponse() instead of the @ApiResponse() decorator

--- transcendence-app/src/oauth42/oauth42.controller.ts
+++ transcendence-app/src/oauth42/oauth42.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
-import { ApiBadGatewayResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  ApiBadGatewayResponse,
+  ApiFoundResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { User } from '../user/decorators/user.decorator';
 import { UserDto } from '../user/dto/user.dto';
 import { OAuth42Guard } from './oauth42.guard';
@@ -8,7 +12,7 @@ import { OAuth42Guard } from './oauth42.guard';
 @Controller('oauth42')
 export class OAuth42Controller {
   @Get()
-  @ApiResponse({ status: HttpStatus.FOUND })
+  @ApiFoundResponse()
   @ApiBadGatewayResponse()
   @UseGuards(OAuth42Guard)
   async oauth42Login(@User() user: UserDto) {
