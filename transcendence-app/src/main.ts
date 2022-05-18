7e980ea88143c5e92e1443e6721350b8f68b90f3
gbudau <54190400+gbudau@users.noreply.github.com>
2022-05-18 16:54:55 +0200

    #36/research about open api as a way to define our api (#39)
    * Update package.json
    
    Add swagger
    
    * Add swagger
    
    * Update OAuth42Controller
    
    Use the @ApiFoundResponse() instead of the @ApiResponse() decorator

--- transcendence-app/src/main.ts
+++ transcendence-app/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,13 @@ async function bootstrap() {
     defaultVersion: '1',
   });
   app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder()
+    .setTitle('transcendence-app')
+    .setDescription('The transcendence-app API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- transcendence-app/src/oauth42/oauth42.controller.ts
+++ transcendence-app/src/oauth42/oauth42.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  ApiBadGatewayResponse,
+  ApiFoundResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { User } from '../user/decorators/user.decorator';
 import { UserDto } from '../user/dto/user.dto';
 import { OAuth42Guard } from './oauth42.guard';
 
+@ApiTags('oauth42')
 @Controller('oauth42')
 export class OAuth42Controller {
   @Get()
+  @ApiFoundResponse()
+  @ApiBadGatewayResponse()
   @UseGuards(OAuth42Guard)
   async oauth42Login(@User() user: UserDto) {
     return user;
