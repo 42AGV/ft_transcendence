9c3982a8e8249c9e68b9a47d818cb33c47ea736a
Victor Dedios <victor.dedios@outlook.com>
2022-05-23 19:57:21 +0200

    make username and email unique (#27)

--- db/setup/schema.sql
+++ db/setup/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE ft_transcendence;
 
 CREATE TABLE IF NOT EXISTS users(
     UUID        UUID DEFAULT gen_random_uuid()  PRIMARY KEY,
-    USERNAME    VARCHAR(20)                     NOT NULL,
-    EMAIL       VARCHAR(50)                     NOT NULL,
-    IMG         VARCHAR(150)                    NOT NULL
+    USERNAME    VARCHAR(20)              NOT NULL UNIQUE,
+    EMAIL       VARCHAR(50)              NOT NULL UNIQUE,
+    IMG         VARCHAR(150)
 );
