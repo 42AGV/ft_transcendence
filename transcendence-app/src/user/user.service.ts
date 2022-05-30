a503511db8400e546d3f0a2e2b75c5fc03aef43d
fizcamposancos <fizcamposancos@gmail.com>
2022-05-31 00:08:43 +0200

    Change: return value always an array of users. Fix pagination

--- transcendence-app/src/user/user.service.ts
+++ transcendence-app/src/user/user.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UserEntity } from './user.entity';
 import { v4 as uuidv4 } from 'uuid';
-import {
-  MAX_USER_ENTRIES_PER_PAGE,
-  UsersPaginationQueryDto,
-} from './dto/user.pagination.dto';
+import { UsersPaginationQueryDto } from './dto/user.pagination.dto';
+
+const MAX_USER_ENTRIES_PER_PAGE = 20;
 
 @Injectable()
 export class UserService {
@@ -19,27 +18,36 @@ export class UserService {
     return this.users.find((user) => user.id === id) || null;
   }
   retrieveUsers(queryDto: UsersPaginationQueryDto) {
-    if (queryDto.limit || queryDto.offset) {
-      let ret: string[] = [];
-      queryDto.limit = queryDto.limit
-        ? queryDto.limit
-        : MAX_USER_ENTRIES_PER_PAGE;
-      queryDto.offset = queryDto.offset ? queryDto.offset : 0;
-      ret = this.users
-        .filter((user, i) => i >= queryDto.offset && i < queryDto.limit)
-        .map((user) => user.username);
-      return queryDto.sort ? ret.sort() : ret;
+    const limit = queryDto.limit ?? MAX_USER_ENTRIES_PER_PAGE;
+    const offset = queryDto.offset ?? 0;
+    const usersCopy = [...this.users];
+
+    if (queryDto.sort) {
+      usersCopy.sort((a, b) =>
+        a.username > b.username ? 1 : a.username === b.username ? 0 : -1,
+      );
     }
-    return this.users;
+
+    return usersCopy.slice(offset, offset + limit);
   }
 
   private create(user: UserDto) {
-    const userEntity: UserEntity = {
+    let userEntity: UserEntity = {
       id: uuidv4(),
       createdAt: new Date(Date.now()),
       ...user,
     };
-    this.users.push(userEntity);
+
+    for (let index = 0; index < 50; index++) {
+      userEntity = {
+        id: uuidv4(),
+        createdAt: new Date(Date.now()),
+        username: user.username + index,
+        avatar: user.avatar,
+        email: user.email,
+      };
+      this.users.push(userEntity);
+    }
     return userEntity;
   }
 
