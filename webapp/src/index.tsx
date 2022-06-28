faeff26dca2484ad23e95e72bb3d93f65a6381fa
gbudau <54190400+gbudau@users.noreply.github.com>
2022-06-28 13:58:29 +0200

    Add barrel file for webapp pages
    Remove barrel file for Icon component

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import './index.css';
-import Landing from './pages/Landing';
 import reportWebVitals from './reportWebVitals';
+import { Landing } from './pages';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
