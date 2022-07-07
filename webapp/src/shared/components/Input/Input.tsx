882533037640db19b265fbc116bd45cace54088f
fizcamposancos <fizcamposancos@gmail.com>
2022-07-07 16:53:34 +0200

    input form trials

---
+++ webapp/src/shared/components/Input/Input.tsx
@@ -0,0 +1,22 @@
+import { IconVariant } from '../Icon/Icon';
+
+export enum InputColor {
+  LIGHT = 'color-light',
+  DARK = 'color-dark',
+}
+
+type InputProps = {
+  color: InputColor;
+  label?: string;
+  iconVariant?: IconVariant;
+  input?: string;
+};
+
+export default function Input({
+  color,
+  label,
+  iconVariant,
+  input,
+}: InputProps) {
+  return <form className="input-form"></form>;
+}
