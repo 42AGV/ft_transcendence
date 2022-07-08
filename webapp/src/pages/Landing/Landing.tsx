08cca0c0f0aff8b624abc66f6639a47a94f30ba4
fizcamposancos <56964234+fizcamposancos@users.noreply.github.com>
2022-07-08 23:32:19 +0200

    #53/Create button UI component (#89)
    * create button component
    
    * Changes with suggestions accepted:
    cursor not allowed, variable names, template literal, disabled selector, inline validation for icon component, div instead of grid
    
    * Merge with new text css classnames
    
    * change Icon with IconVariant

--- webapp/src/pages/Landing/Landing.tsx
+++ webapp/src/pages/Landing/Landing.tsx
@@ -1,4 +1,6 @@
 import {
+  Button,
+  ButtonVariant,
   Icon,
   IconSize,
   IconVariant,
@@ -11,8 +13,29 @@ import { Color } from '../../shared/types';
 import './Landing.css';
 
 export default function Landing() {
+  const btn_action = (): void => alert('This is an alert');
+  const btn_link = (): void => {
+    window.location.href = 'http://google.com';
+  };
   return (
     <section className="Landing">
+      <Button variant={ButtonVariant.SUBMIT} onClick={btn_action}>
+        ALERT
+      </Button>
+      <Button
+        variant={ButtonVariant.WARNING}
+        iconVariant={IconVariant.LOGOUT}
+        disabled
+      >
+        LOGOUT BUTTON
+      </Button>
+      <Button
+        variant={ButtonVariant.SUBMIT}
+        iconVariant={IconVariant.ARROW_FORWARD}
+        onClick={btn_link}
+      >
+        To Google
+      </Button>
       <Icon
         variant={IconVariant.PLAY}
         color={Color.ONLINE}
