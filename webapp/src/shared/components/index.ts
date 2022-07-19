55a54571207cd0caec0acdf29a6568a9eff8ac4c
gbudau <54190400+gbudau@users.noreply.github.com>
2022-07-19 22:09:28 +0200

    #59 Create header UI component (#96)
    * #59 Create header UI component
    
    Wrap the Icon/Avatar ternary in a Link
    
    * Update Status component
    Change display to inline-flex
    Change the type of HeaderProps navigation to string
    
    * Update Header.css
    Add flexbox to the navigation anchor and remove flexbox from the SVG and image
    
    * Update Header
    
    Remove the div wrapper around the navigation link
    Add the header-navigation className to the link
    Add a div wrapper around the Link to enable click navigation only for the Avatar and Icon
    
    Use the Text component for the Header text
    
    * Update index.css
    Match text font sizes with Figma
    Scale the font sizes for desktop in media query
    
    
    * Update HeaderProps
    Add IconVariant type to navigationFigure
    
    Co-authored-by: Victor Dedios <57688116+vdedios@users.noreply.github.com>

--- webapp/src/shared/components/index.ts
+++ webapp/src/shared/components/index.ts
@@ -8,9 +8,12 @@ export { default as Button } from './Button/Button';
 export * from './Button/Button';
 
 export { default as Status } from './Status/Status';
+export * from './Status/Status';
 
 export { default as NavigationItem } from './NavigationItem/NavigationItem';
 
 export * from './Avatar/Avatar';
 
 export { default as NavigationBar } from './NavigationBar/NavigationBar';
+
+export { default as Header } from './Header/Header';
