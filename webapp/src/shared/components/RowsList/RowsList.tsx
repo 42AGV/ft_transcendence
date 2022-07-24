49ece3f208837b6aff34bb12032ebe9c866c431a
Alvaro Ollero <aollero@gmail.com>
2022-07-24 11:09:00 +0300

    we accept no rows at all in the rowslist component

--- webapp/src/shared/components/RowsList/RowsList.tsx
+++ webapp/src/shared/components/RowsList/RowsList.tsx
@@ -2,15 +2,13 @@ import './RowsList.css';
 import Row, { RowProps } from '../Row/Row';
 
 export type RowsListProps = {
-  rows: RowProps[];
+  rows?: RowProps[];
 };
 
 export default function RowsList({ rows }: RowsListProps) {
   return (
     <div className="rows-list-wrapper">
-      {rows.map((rowProps) => (
-        <Row {...rowProps} />
-      ))}
+      {rows && rows.map((rowProps) => <Row {...rowProps} />)}
     </div>
   );
 }
