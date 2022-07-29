a35ec33dfad35bffaa205819783cea3d8bc15d95
fizcamposancos <56964234+fizcamposancos@users.noreply.github.com>
2022-07-29 22:22:36 +0200

    #61/create form input component (#104)
    * initial commit
    
    * input form trials
    
    * form in one color and without submit
    
    * With form tag commented and more or less working
    
    * uncontrolled input form
    
    * changes requested half done
    
    * added default color to labels

--- webapp/src/shared/components/index.ts
+++ webapp/src/shared/components/index.ts
@@ -4,6 +4,9 @@ export * from './Icon/Icon';
 export { default as Text } from './Text/Text';
 export * from './Text/Text';
 
+export { default as Input } from './Input/Input';
+export * from './Input/Input';
+
 export { default as Button } from './Button/Button';
 export * from './Button/Button';
 
