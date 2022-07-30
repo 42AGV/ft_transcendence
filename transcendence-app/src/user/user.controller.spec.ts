334b8a0e991fdfdbd2bc12d251136c475fe273f9
gbudau <54190400+gbudau@users.noreply.github.com>
2022-07-30 20:36:39 +0200

    Read from config in PostgreSQL connection
    Add environment variables for PostgreSQL connection
    Add docker-compose.test.yml for e2e tests
    Update package.json add pretest and post:test scripts to start the database container
    Move auth and user e2e tests to transcendence-app/test directory
    Remove app.e2e-spec.ts

--- transcendence-app/src/user/user.controller.spec.ts
+++ transcendence-app/src/user/user.controller.spec.ts
@@ -1,12 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
-import { HttpStatus, INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { UserModule } from './user.module';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
 import { NotFoundException } from '@nestjs/common';
-import { AuthenticatedGuard } from '../shared/guards/authenticated.guard';
 import { v4 as uuidv4 } from 'uuid';
 
 const testUserDto: UserDto = {
@@ -15,67 +11,6 @@ const testUserDto: UserDto = {
   avatar: 'www.example.jpg',
 };
 const testUserId = uuidv4();
-
-describe('User Controller end to end test', () => {
-  let app: INestApplication;
-  const canActivate = jest.fn(() => true);
-
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [UserModule],
-      controllers: [UserController],
-    })
-      .overrideGuard(AuthenticatedGuard)
-      .useValue({ canActivate })
-      .compile();
-
-    app = module.createNestApplication();
-    await app.init();
-  });
-
-  it('should create a user', async () => {
-    const server = await app.getHttpServer();
-    const response = await request(server)
-      .post('/users')
-      .send(testUserDto)
-      .expect(HttpStatus.CREATED);
-    await request(server)
-      .get(`/users/${response.body.id}`)
-      .expect(HttpStatus.OK);
-    await request(server)
-      .get(`/users/${testUserId}`)
-      .expect(HttpStatus.NOT_FOUND);
-  });
-
-  it('should return an array of usernames', async () => {
-    const server = await app.getHttpServer();
-    for (let i = 0; i < 50; i++) {
-      await request(server)
-        .post('/users')
-        .send({
-          username: 'user' + i,
-          email: 'afgv@github.com',
-          avatar: 'www.example.jpg',
-        });
-    }
-    const response = await request(server)
-      .get('/users/?limit=20&offset=0&sort=true')
-      .expect(HttpStatus.OK);
-    expect(response.body.length).toBe(20);
-    await request(server).get('/users/').expect(HttpStatus.OK);
-  });
-
-  it('returns forbidden if guard fails', () => {
-    canActivate.mockReturnValueOnce(false);
-    return request(app.getHttpServer())
-      .get('/users/me')
-      .expect(HttpStatus.FORBIDDEN);
-  });
-
-  afterAll(async () => {
-    await app.close();
-  });
-});
 
 describe('User Controller unit tests', () => {
   let controller: UserController;
---
+++ transcendence-app/test/user/user.e2e-spec.ts
@@ -0,0 +1,95 @@
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { ConfigModule } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserModule } from '../../src/user/user.module';
+import { v4 as uuidv4 } from 'uuid';
+import { validate } from '../../src/config/env.validation';
+import { UserController } from '../../src/user/user.controller';
+import { AuthenticatedGuard } from '../../src/shared/guards/authenticated.guard';
+import { UserDto } from '../../src/user/dto/user.dto';
+
+const testUserDto: UserDto = {
+  username: 'user',
+  email: 'afgv@github.com',
+  avatar: 'www.example.jpg',
+};
+const testUserId = uuidv4();
+
+describe('[Feature] User - /users', () => {
+  let app: INestApplication;
+  const canActivate = jest.fn(() => true);
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        UserModule,
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+          isGlobal: true,
+          cache: true,
+          validate,
+        }),
+      ],
+      controllers: [UserController],
+    })
+      .overrideGuard(AuthenticatedGuard)
+      .useValue({ canActivate })
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+        forbidUnknownValues: true,
+      }),
+    );
+    await app.init();
+  });
+
+  it('Creates an user [POST /]', async () => {
+    const server = await app.getHttpServer();
+    const response = await request(server)
+      .post('/users')
+      .send(testUserDto)
+      .expect(HttpStatus.CREATED);
+    await request(server)
+      .get(`/users/${response.body.id}`)
+      .expect(HttpStatus.OK);
+    await request(server)
+      .get(`/users/${testUserId}`)
+      .expect(HttpStatus.NOT_FOUND);
+  });
+
+  it('Get all users [GET /]', async () => {
+    const server = await app.getHttpServer();
+    for (let i = 0; i < 50; i++) {
+      await request(server)
+        .post('/users')
+        .send({
+          username: 'user' + i,
+          email: i + 'afgv@github.com',
+          avatar: 'www.example.jpg',
+        });
+    }
+    const response = await request(server)
+      .get('/users/?limit=20&offset=0&sort=true')
+      .expect(HttpStatus.OK);
+    expect(response.body.length).toBe(20);
+  });
+
+  it('Get current user [GET /me]', () => {
+    canActivate.mockReturnValueOnce(false);
+    return request(app.getHttpServer())
+      .get('/users/me')
+      .expect(HttpStatus.FORBIDDEN);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
