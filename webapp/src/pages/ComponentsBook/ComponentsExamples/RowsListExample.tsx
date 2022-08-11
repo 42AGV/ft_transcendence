caa37d38fe65676bd5711900f2c69340ee920bd3
Alvaro Ollero <aollero@gmail.com>
2022-08-11 21:03:46 +0300

    why do I use object as generic argument

--- webapp/src/pages/ComponentsBook/ComponentsExamples/RowsListExample.tsx
+++ webapp/src/pages/ComponentsBook/ComponentsExamples/RowsListExample.tsx
@@ -5,7 +5,7 @@ const randomAvatar = 'https://i.pravatar.cc/1000';
 const randomAvatar2 = 'https://i.pravatar.cc/2000';
 const randomAvatar3 = 'https://i.pravatar.cc/3000';
 
-export const buttonAction = (): void => alert('This is an alert');
+const buttonAction = (): void => alert('This is an alert');
 const buttonLink = (): void => {
   window.location.href = 'https://google.com';
 };
