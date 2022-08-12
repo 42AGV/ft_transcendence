92db9386152f99e34bd9eaaaf2954979de61750c
Alvaro Ollero <aollero@gmail.com>
2022-08-12 21:19:50 +0300

    addressed review

--- webapp/src/pages/Users/Users.tsx
+++ webapp/src/pages/Users/Users.tsx
@@ -17,26 +17,12 @@ import {
 } from '../../shared/urls';
 import { Link } from 'react-router-dom';
 
-class User {
+interface User {
   username: string;
   email: string;
   avatarId: string | null;
   id: string;
   createdAt: Date;
-
-  constructor(
-    username: string,
-    email: string,
-    avatarId: string | null,
-    id: string,
-    createdAt: Date,
-  ) {
-    this.username = username;
-    this.email = email;
-    this.avatarId = avatarId;
-    this.id = id;
-    this.createdAt = createdAt;
-  }
 }
 
 export default function Users() {
