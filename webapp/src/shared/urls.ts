f7313e0c00261fc1f786cf7529260c71924835ec
Damrod <aollero@gmail.com>
2022-08-12 21:24:14 +0300

    Create users page (#108)
    Users page added and connected to backend

--- webapp/src/shared/urls.ts
+++ webapp/src/shared/urls.ts
@@ -1,4 +1,8 @@
+const API_ENDPOINT_URL = '/api/v1';
+export const USER_URL = '/user';
 export const USERS_URL = '/users';
+export const USERS_EP_URL = `${API_ENDPOINT_URL}${USERS_URL}`;
 export const PLAY_URL = '/play';
 export const CHAT_URL = '/chat';
 export const COMPONENTS_BOOK_URL = '/components-book';
+export const WILDCARD_AVATAR_URL = 'https://i.pravatar.cc/9000';
