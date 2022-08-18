586c05182fbd429b76d2233103fc302aaccb643d
Alvaro Ollero <aollero@gmail.com>
2022-08-18 13:59:47 +0300

    some problem rendering avatar small/large in component book: coordinates not synced + need to fix warning in console in comp book

--- webapp/src/pages/ComponentsBook/ComponentsExamples/EditableAvatarExample.tsx
+++ webapp/src/pages/ComponentsBook/ComponentsExamples/EditableAvatarExample.tsx
@@ -1,13 +1,32 @@
 import { BookSection } from '../BookSection';
 import { WILDCARD_AVATAR_URL } from '../../../shared/urls';
 import { EditableAvatar } from '../../../shared/components/Avatar/EditableAvatar';
+import { LargeAvatar, SmallAvatar } from '../../../shared/components';
 
-const randomAvatar = WILDCARD_AVATAR_URL;
+const randomAvatar2 = WILDCARD_AVATAR_URL;
 // const randomAvatar = 'http://localhost/icons/9000.jpeg';
-const randomAvatar2 = 'http://localhost/icons/grid.jpg';
+const randomAvatar = 'http://localhost/icons/grid.jpg';
 
-export const EditableAvatarExample = () => (
-  <BookSection title="Editable Avatar component">
-    <EditableAvatar url={randomAvatar} XCoordinate={13} YCoordinate={50} />
-  </BookSection>
-);
+export const EditableAvatarExample = () => {
+  const xCoord = 10;
+  const yCoord = 15;
+  return (
+    <BookSection title="Editable Avatar component">
+      <EditableAvatar
+        url={randomAvatar}
+        XCoordinate={xCoord}
+        YCoordinate={yCoord}
+      />
+      <LargeAvatar
+        url={randomAvatar}
+        XCoordinate={xCoord}
+        YCoordinate={yCoord}
+      />
+      <SmallAvatar
+        url={randomAvatar}
+        XCoordinate={xCoord}
+        YCoordinate={yCoord}
+      />
+    </BookSection>
+  );
+};
