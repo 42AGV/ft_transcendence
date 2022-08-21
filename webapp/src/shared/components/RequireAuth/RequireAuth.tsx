070f4fe8e6ab301b4c9375f21c25a2a3582f2882
gbudau <54190400+gbudau@users.noreply.github.com>
2022-08-21 17:19:38 +0200

    Update RequireAuth
    Improve the style of the loading text

--- webapp/src/shared/components/RequireAuth/RequireAuth.tsx
+++ webapp/src/shared/components/RequireAuth/RequireAuth.tsx
@@ -1,13 +1,21 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/UseAuth';
+import { Text, TextVariant, TextWeight } from '../';
+import './RequireAuth.css';
 
 export default function RequireAuth({ children }: { children: JSX.Element }) {
   const auth = useAuth();
   const location = useLocation();
 
   if (auth.loading) {
-    // TODO: Improve style or create Loading component
-    return <div>Loading...</div>;
+    // TODO: Replace with Loading component
+    return (
+      <div className="require-auth">
+        <Text variant={TextVariant.SUBHEADING} weight={TextWeight.MEDIUM}>
+          Loading...
+        </Text>
+      </div>
+    );
   }
 
   if (!auth.user) {
