54005b2f734fcd790aa2b9df81e385e41d53b56a
Alvaro Ollero <aollero@gmail.com>
2022-08-23 11:27:07 +0300

    removed some comments

--- webapp/src/pages/ComponentsBook/ComponentsExamples/EditableAvatarExample.tsx
+++ webapp/src/pages/ComponentsBook/ComponentsExamples/EditableAvatarExample.tsx
@@ -4,8 +4,6 @@ import { EditableAvatar } from '../../../shared/components/Avatar/EditableAvatar
 import { LargeAvatar, SmallAvatar } from '../../../shared/components';
 
 const randomAvatar = WILDCARD_AVATAR_URL;
-//const randomAvatar = 'http://localhost/icons/9000.jpeg';
-//const randomAvatar = 'http://localhost/icons/grid.jpg';
 
 export const EditableAvatarExample = () => {
   const xCoord = 20.14;
