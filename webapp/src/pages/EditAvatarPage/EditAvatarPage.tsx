fe7b4b022074c969f7df3500658d67f0fe995493
Alvaro Ollero <aollero@gmail.com>
2022-08-24 18:24:51 +0300

    draft of actual content

--- webapp/src/pages/EditAvatarPage/EditAvatarPage.tsx
+++ webapp/src/pages/EditAvatarPage/EditAvatarPage.tsx
@@ -1,9 +1,10 @@
 import './EditAvatarPage.css';
 import {
-  EditUserForm,
+  Button,
+  ButtonVariant,
   Header,
   IconVariant,
-  LargeAvatar,
+  Row,
   Text,
   TextVariant,
   TextWeight,
@@ -19,12 +20,14 @@ import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { instanceOfUser, User } from '../../shared/generated';
 import { usersApi } from '../../shared/services/ApiService';
+import { EditableAvatar } from '../../shared/components/Avatar/EditableAvatar';
 
 export default function EditAvatarPage() {
   const getCurrentUser = useCallback(
     () => usersApi.userControllerGetCurrentUser(),
     [],
   );
+  const submitChanges = useCallback(() => true, []);
   const [user, setUser] = useState<User | null>(null);
   const { data } = useData<User>(getCurrentUser);
   const navigate = useNavigate();
@@ -49,18 +52,25 @@ export default function EditAvatarPage() {
       >
         edit profile
       </Header>
-      <div className="edit-avatar">
-        <LargeAvatar
-          url={
-            user.avatarId
-              ? `${USERS_EP_URL}/${user.id}/avatar`
-              : WILDCARD_AVATAR_URL
-          }
-          edit={true}
-          editUrl={EDIT_AVATAR_URL}
-        />
-      </div>
-      <EditUserForm origFullName={user.fullName} origEmail={user.email} />
+      <Row
+        iconVariant={IconVariant.FILE}
+        onClick={submitChanges}
+        title="Select file"
+      />
+      <EditableAvatar
+        url={
+          user.avatarId
+            ? `${USERS_EP_URL}/${user.id}/avatar`
+            : WILDCARD_AVATAR_URL
+        }
+      />
+      <Button
+        variant={ButtonVariant.SUBMIT}
+        iconVariant={IconVariant.ARROW_FORWARD}
+        onClick={submitChanges}
+      >
+        Upload
+      </Button>
     </div>
   );
 }
