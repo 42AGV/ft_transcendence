24611ab6880e024b6ab815272174f11407ae3012
Victor De dios mejia <vde-dios@c1r11s4.42madrid.com>
2022-09-18 18:54:14 +0200

    move text out of switch component

--- webapp/src/pages/ComponentsBookPage/ComponentsExamples/ToggleSwitchExample.tsx
+++ webapp/src/pages/ComponentsBookPage/ComponentsExamples/ToggleSwitchExample.tsx
@@ -1,14 +1,28 @@
 import { useState } from 'react';
-import { ToggleSwitch } from '../../../shared/components';
+import {
+  ToggleSwitch,
+  Text,
+  TextColor,
+  TextVariant,
+  TextWeight,
+} from '../../../shared/components';
 import { BookSection } from '../BookSection';
 
 export const ToggleSwitchExample = () => {
   const [isToggled, setIsToggled] = useState(false);
   const onToggle = () => setIsToggled(!isToggled);
+  const value = isToggled ? 'toggled' : 'not toggled';
+
   return (
     <BookSection title="Chat Bubble component">
-      <ToggleSwitch value="value" isToggled={false} onToggle={onToggle} />
-      {/* <ToggleSwitch value="value" /> */}
+      <ToggleSwitch isToggled={isToggled} onToggle={onToggle} />
+      <Text
+        variant={TextVariant.HEADING}
+        color={TextColor.LIGHT}
+        weight={TextWeight.BOLD}
+      >
+        {value}
+      </Text>
     </BookSection>
   );
 };
--- webapp/src/shared/components/ToggleSwitch/ToggleSwitch.tsx
+++ webapp/src/shared/components/ToggleSwitch/ToggleSwitch.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
-import Text, { TextColor, TextVariant, TextWeight } from '../Text/Text';
 import './ToggleSwitch.css';
 
 type ToggleSwitchProps = {
-  value?: string;
   isToggled: boolean;
   onToggle: React.ChangeEventHandler<HTMLInputElement>;
 };
 
 export default function ToggleSwitch({
-  value,
   isToggled,
   onToggle,
 }: ToggleSwitchProps) {
@@ -19,46 +15,6 @@ export default function ToggleSwitch({
         <input type="checkbox" checked={isToggled} onChange={onToggle} />
         <span className="switch" />
       </label>
-      <div className="toggle-switch-text">
-        {value && (
-          <Text
-            variant={TextVariant.HEADING}
-            color={TextColor.LIGHT}
-            weight={TextWeight.BOLD}
-          >
-            {value}
-          </Text>
-        )}
-      </div>
     </div>
   );
 }
-
-// This version works
-// type ToggleSwitchProps = {
-//   value?: string;
-// };
-
-// export default function ToggleSwitch({ value }: ToggleSwitchProps) {
-//   const [isToggled, setIsToggled] = useState(false);
-//   const onToggle = () => setIsToggled(!isToggled);
-//   return (
-//     <div className="toggle-switch">
-//       <label className="toggle-switch-label">
-//         <input type="checkbox" checked={isToggled} onChange={onToggle} />
-//         <span className="switch" />
-//       </label>
-//       <span className="toggle-switch-text">
-//         {value && (
-//           <Text
-//             variant={TextVariant.HEADING}
-//             color={TextColor.LIGHT}
-//             weight={TextWeight.BOLD}
-//           >
-//             {value}
-//           </Text>
-//         )}
-//       </span>
-//     </div>
-//   );
-// }
