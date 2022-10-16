ca10337a28d5fcb721c5d53f2d4c27d2059a7690
vdedios <victor.dedios@outlook.com>
2022-10-16 19:23:54 +0200

    remove unnecesary empty validation for userAvatar

--- transcendence-app/src/user/dto/user.avatar.dto.ts
+++ transcendence-app/src/user/dto/user.avatar.dto.ts
@@ -1,9 +1,8 @@
 import { StreamableFile } from '@nestjs/common';
-import { IsNotEmpty, IsUUID, ValidateIf } from 'class-validator';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class UserAvatarDto {
   @IsUUID()
-  @ValidateIf((object, value) => value !== null)
   avatarId!: string;
 
   @IsNotEmpty()
