022ea8ad5e41e381634059524679f6a6d87f263f
gbudau <54190400+gbudau@users.noreply.github.com>
2022-10-19 15:31:23 +0200

    Update MessageInput
    Remove the div ref

--- webapp/src/pages/ChatRoom/components/MessageInput/MessageInput.tsx
+++ webapp/src/pages/ChatRoom/components/MessageInput/MessageInput.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, ButtonVariant } from '../../../../shared/components';
 import socket from '../../../../shared/socket';
 import './MessageInput.css';
@@ -9,27 +9,23 @@ type MessageInputProps = {
 
 const MessageInput = ({ to }: MessageInputProps) => {
   const [value, setValue] = useState('');
-  const divRef = useRef<HTMLDivElement>(null);
 
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     socket.emit('message', { roomId: to, content: value });
     setValue('');
-    if (divRef.current) {
-      divRef.current.dataset.replicatedValue = '';
-    }
   };
 
   const handleOnInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
-    if (divRef.current) {
-      divRef.current.dataset.replicatedValue = e.currentTarget.value;
-      setValue(e.currentTarget.value);
-    }
+    setValue(e.currentTarget.value);
   };
 
   return (
     <form className="message-input-form" onSubmit={handleOnSubmit}>
-      <div className="message-input-grow-wrap paragraph-regular" ref={divRef}>
+      <div
+        className="message-input-grow-wrap paragraph-regular"
+        data-replicated-value={value}
+      >
         <textarea
           className="message-input-text paragraph-regular"
           placeholder="Type your message"
