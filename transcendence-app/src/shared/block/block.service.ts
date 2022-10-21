efe57a4f39b79b0acfed044a403f8e5803810f5b
gbudau <54190400+gbudau@users.noreply.github.com>
2022-10-21 17:44:01 +0200

    #214/Create an endpoint to block a user

--- transcendence-app/src/shared/block/block.service.ts
+++ transcendence-app/src/shared/block/block.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { IBlockRepository } from './infrastructure/block.repository';
 
 @Injectable()
-export class BlockService {}
+export class BlockService {
+  constructor(private blockRepository: IBlockRepository) {}
+
+  addBlock(blockerId: string, blockedId: string) {
+    return this.blockRepository.addBlock({ blockerId, blockedId });
+  }
+}
--- transcendence-app/src/user/user.module.ts
+++ transcendence-app/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { DbModule } from '../shared/db/db.module';
 import { LocalFileModule } from '../shared/local-file/local-file.module';
+import { BlockModule } from '../shared/block/block.module';
 
 @Module({
-  imports: [DbModule, LocalFileModule],
+  imports: [DbModule, LocalFileModule, BlockModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
