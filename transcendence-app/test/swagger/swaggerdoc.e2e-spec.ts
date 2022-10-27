f87f9adc16dc6453345d47feb5b99a02087c15ed
Alvaro Ollero <aollero@gmail.com>
2022-10-27 21:08:00 +0300

    lint

--- transcendence-app/test/swagger/swaggerdoc.e2e-spec.ts
+++ transcendence-app/test/swagger/swaggerdoc.e2e-spec.ts
@@ -1,42 +1,45 @@
 // my-api.e2e-spec.ts
-import { Test, TestingModule } from "@nestjs/testing";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import * as fs from "fs";
-import { validate } from "../../src/config/env.validation";
-import { ClassSerializerInterceptor, INestApplication, ValidationPipe } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { UserModule } from "../../src/user/user.module";
-import { UserController } from "../../src/user/user.controller";
-import { AuthenticatedGuard } from "../../src/shared/guards/authenticated.guard";
-import * as yaml from "yaml";
-import { AuthModule } from "../../src/auth/auth.module";
-import { SocketModule } from "../../src/socket/socket.module";
-import { ChatModule } from "../../src/chat/chat.module";
-import { APP_INTERCEPTOR } from "@nestjs/core";
-import { ChatController } from "../../src/chat/chat.controller";
-import { AuthController } from "../../src/auth/auth.controller";
-import { AuthService } from "../../src/auth/auth.service";
-import { SocketService } from "../../src/socket/socket.service";
-import { LocalFileService } from "../../src/shared/local-file/local-file.service";
-import { LocalFileModule } from "../../src/shared/local-file/local-file.module";
-import { ChatService } from "../../src/chat/chat.service";
-import { ChatMemberService } from "../../src/chat/chatmember.service";
-import { DbModule } from "../../src/shared/db/db.module";
+import { Test, TestingModule } from '@nestjs/testing';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { validate } from '../../src/config/env.validation';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from '../../src/user/user.module';
+import { UserController } from '../../src/user/user.controller';
+import { AuthenticatedGuard } from '../../src/shared/guards/authenticated.guard';
+import * as yaml from 'yaml';
+import { AuthModule } from '../../src/auth/auth.module';
+import { SocketModule } from '../../src/socket/socket.module';
+import { ChatModule } from '../../src/chat/chat.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ChatController } from '../../src/chat/chat.controller';
+import { AuthController } from '../../src/auth/auth.controller';
+import { AuthService } from '../../src/auth/auth.service';
+import { SocketService } from '../../src/socket/socket.service';
+import { LocalFileService } from '../../src/shared/local-file/local-file.service';
+import { LocalFileModule } from '../../src/shared/local-file/local-file.module';
+import { ChatService } from '../../src/chat/chat.service';
+import { ChatMemberService } from '../../src/chat/chatmember.service';
+import { DbModule } from '../../src/shared/db/db.module';
 
-describe("[Feature] User - /users", () => {
+describe('[Feature] User - /users', () => {
   let app: INestApplication;
   const canActivate = jest.fn(() => true);
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule
-
-    ({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({
-          envFilePath: ".env.test",
+          envFilePath: '.env.test',
           isGlobal: true,
           cache: true,
-          validate
+          validate,
         }),
         UserModule,
         AuthModule,
@@ -48,18 +51,15 @@ describe("[Feature] User - /users", () => {
       providers: [
         {
           provide: APP_INTERCEPTOR,
-          useClass: ClassSerializerInterceptor
+          useClass: ClassSerializerInterceptor,
         },
         AuthService,
         ChatService,
-        ChatMemberService
+        ChatMemberService,
       ],
-      controllers: [
-        AuthController,
-        UserController,
-        ChatController
-      ]
-    }).overrideGuard(AuthenticatedGuard)
+      controllers: [AuthController, UserController, ChatController],
+    })
+      .overrideGuard(AuthenticatedGuard)
       .useValue({ canActivate })
       .compile();
     app = moduleFixture.createNestApplication();
@@ -68,23 +68,28 @@ describe("[Feature] User - /users", () => {
         whitelist: true,
         transform: true,
         transformOptions: {
-          enableImplicitConversion: true
+          enableImplicitConversion: true,
         },
-        forbidUnknownValues: true
-      })
+        forbidUnknownValues: true,
+      }),
     );
+    app.setGlobalPrefix('api');
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: '1',
+    });
     await app.init();
   });
 
-  it("should generate swagger spec", async () => {
+  it('should generate swagger spec', async () => {
     const options = new DocumentBuilder()
-      .setTitle("transcendence-app")
-      .setDescription("The transcendence-app API description")
-      .setVersion("1.0")
+      .setTitle('transcendence-app')
+      .setDescription('The transcendence-app API description')
+      .setVersion('1.0')
       .build();
     const document = SwaggerModule.createDocument(app, options);
     const yamlString: string = yaml.stringify(document, {});
-    fs.writeFileSync("./swagger-spec.yaml", yamlString);
+    fs.writeFileSync('./swagger-spec.yaml', yamlString);
   });
 
   afterAll(async () => {
