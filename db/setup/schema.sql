1db32078de1443ab2573ac97002be34927944a7b
Alvaro Ollero <aollero@gmail.com>
2022-10-27 10:15:56 +0300

    reverted whitespace divergence from master

--- db/setup/schema.sql
+++ db/setup/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE
-    IF NOT EXISTS LocalFile(
+  IF NOT EXISTS LocalFile(
     "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     "filename" VARCHAR(100) NOT NULL UNIQUE,
     "path" VARCHAR(255) NOT NULL UNIQUE,
@@ -9,7 +9,7 @@ CREATE TABLE
   );
 
 CREATE TABLE
-    IF NOT EXISTS Users(
+  IF NOT EXISTS Users(
     "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     "username" VARCHAR(20) NOT NULL UNIQUE,
     "email" VARCHAR(50) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE
   IF NOT EXISTS AuthProvider (
     "providerId" TEXT NOT NULL,
     "provider" ProviderType NOT NULL,
-    "userId" UUID REFERENCES Users (id) ON DELETE CASCADE,
+    "userId" UUID REFERENCES Users(id),
     PRIMARY KEY ("providerId", "provider")
   );
 
