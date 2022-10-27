e8b6dc7923cbd112d25af31886b765c382f22d7b
Alvaro Ollero <aollero@gmail.com>
2022-10-27 14:55:01 +0300

    draft of shortened swagger-spec.yaml generation

--- scripts/generate-openapi.sh
+++ scripts/generate-openapi.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH="$( cd -- "$(dirname "$0/..")" >/dev/null 2>&1 ; pwd -P )"
 
- docker run --rm \
+mkdir -p ${SCRIPTPATH}/webapp/src/shared/generated
+docker run --rm \
   -v ${SCRIPTPATH}:/local openapitools/openapi-generator-cli generate \
   -i /local/transcendence-app/swagger-spec.yaml \
   -g typescript-fetch \
