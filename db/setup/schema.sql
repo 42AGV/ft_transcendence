a1882821af37334f131ea98173be03d6a2ec554f
Alvaro Ollero <aollero@gmail.com>
2022-10-27 10:14:34 +0300

    remove changes

--- db/setup/schema.sql
+++ db/setup/schema.sql
@@ -1,24 +1,24 @@
 CREATE TABLE
-  IF NOT EXISTS LocalFile(
-  "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  "filename" VARCHAR(100) NOT NULL UNIQUE,
-  "path" VARCHAR(255) NOT NULL UNIQUE,
-  "mimetype" VARCHAR(255) NOT NULL,
-  "size" BIGINT NOT NULL,
-  "createdAt" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    IF NOT EXISTS LocalFile(
+    "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    "filename" VARCHAR(100) NOT NULL UNIQUE,
+    "path" VARCHAR(255) NOT NULL UNIQUE,
+    "mimetype" VARCHAR(255) NOT NULL,
+    "size" BIGINT NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT NOW()
   );
 
 CREATE TABLE
-  IF NOT EXISTS Users(
-  "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  "username" VARCHAR(20) NOT NULL UNIQUE,
-  "email" VARCHAR(50) NOT NULL,
-  "fullName" VARCHAR(255) NOT NULL,
-  "password" TEXT,
-  "avatarId" UUID REFERENCES LocalFile(id) UNIQUE,
-  "avatarX" SMALLINT DEFAULT 0,
-  "avatarY" SMALLINT DEFAULT 0,
-  "createdAt" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    IF NOT EXISTS Users(
+    "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    "username" VARCHAR(20) NOT NULL UNIQUE,
+    "email" VARCHAR(50) NOT NULL,
+    "fullName" VARCHAR(255) NOT NULL,
+    "password" TEXT,
+    "avatarId" UUID REFERENCES LocalFile(id) UNIQUE,
+    "avatarX" SMALLINT DEFAULT 0,
+    "avatarY" SMALLINT DEFAULT 0,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT NOW()
   );
 
 CREATE TYPE ProviderType AS ENUM ('42');
