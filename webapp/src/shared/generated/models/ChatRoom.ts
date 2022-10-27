74ed55752d86edd6b924d6343b1bfe261de4821b
gbudau <54190400+gbudau@users.noreply.github.com>
2022-10-27 12:41:14 +0200

    #255/Update the Api42Service to fetch the user avatar with updated links (#256)
    * #255/Update the Api42Service to fetch the user avatar with updated links
    
    * Remove webapp/patch.patch
    
    * Update ChatRoom entity and domain
    
    Remove undefined from password type
    
    * Run the openapi-generator script

--- webapp/src/shared/generated/models/ChatRoom.ts
+++ webapp/src/shared/generated/models/ChatRoom.ts
@@ -33,10 +33,10 @@ export interface ChatRoom {
     name: string;
     /**
      * 
-     * @type {object}
+     * @type {string}
      * @memberof ChatRoom
      */
-    password: object;
+    password: string | null;
     /**
      * 
      * @type {string}
