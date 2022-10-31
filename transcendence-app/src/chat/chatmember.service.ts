09974530a690cbe1fc67793668e9d1d28c61959a
Damrod <aollero-@student.42madrid.com>
2022-10-31 23:51:54 +0200

    #265/get endpoint members in chatroom (#290)
    * draft of get chatmembers endpoint
    
    * fixed warnings
    
    * filtering out results of users not currently in the chatroom
    
    * change the param name in the swagger docs
    
    * lint
    
    * Update schema.sql
    
    * ServiceUnav + Forbidd excepts in controller + ownerId as bool in chat room member entity
    
    * lint

--- transcendence-app/src/chat/chatmember.service.ts
+++ transcendence-app/src/chat/chatmember.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { IChatMemberRepository } from './infrastructure/db/chatmember.repository';
 import { ChatMember } from './chatmember.domain';
+import { ChatmemberAsUserResponseDto } from './dto/chatmember.dto';
 
 @Injectable()
 export class ChatMemberService {
   constructor(private chatMemberRepository: IChatMemberRepository) {}
 
-  async addChatmember(
+  async addChatMember(
     chatId: string,
     userId: string,
   ): Promise<ChatMember | null> {
-    const chatmember = {
+    const chatMember = {
       joinedAt: new Date(Date.now()),
       chatId: chatId,
       userId: userId,
@@ -18,7 +19,27 @@ export class ChatMemberService {
       muted: false,
       banned: false,
     };
-    const ret = await this.chatMemberRepository.add(chatmember);
+    const ret = await this.chatMemberRepository.add(chatMember);
     return ret ? new ChatMember(ret) : null;
+  }
+
+  async retrieveChatRoomMembers(
+    chatRoomId: string,
+  ): Promise<ChatmemberAsUserResponseDto[] | null> {
+    const ret = await this.chatMemberRepository.retrieveChatRoomMembers(
+      chatRoomId,
+    );
+
+    return (
+      ret?.map(
+        (chatMember) =>
+          new ChatmemberAsUserResponseDto({
+            username: chatMember.username,
+            avatarId: chatMember.avatarId,
+            avatarX: chatMember.avatarX,
+            avatarY: chatMember.avatarY,
+          }),
+      ) ?? null
+    );
   }
 }
