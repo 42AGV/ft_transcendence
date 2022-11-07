ed5e53c79b862195ce77c1f8052533f7a492cbb2
gbudau <54190400+gbudau@users.noreply.github.com>
2022-11-07 16:28:00 +0100

    Update ChatroomPage
    Check if the chatroom exists and the user is a member of the chatroom

--- webapp/src/pages/ChatroomPage/ChatroomPage.tsx
+++ webapp/src/pages/ChatroomPage/ChatroomPage.tsx
@@ -1,42 +1,87 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { ChatroomMessageInput, ChatroomMessages } from './components';
-import { Header, IconVariant } from '../../shared/components';
+import { Header, IconVariant, Loading } from '../../shared/components';
 import socket from '../../shared/socket';
 import './ChatroomPage.css';
 import { useNavigation } from '../../shared/hooks/UseNavigation';
 import { WsException } from '../../shared/types';
+import { useData } from '../../shared/hooks/UseData';
+import { chatApi } from '../../shared/services/ApiService';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import { useAuth } from '../../shared/hooks/UseAuth';
+import { User } from '../../shared/generated';
 
-function Chatroom() {
+export default function ChatroomPage() {
   const { chatroomId } = useParams();
+  const { authUser } = useAuth();
+
+  if (!chatroomId || !authUser) {
+    return null;
+  }
+  return <Chatroom chatroomId={chatroomId} authUser={authUser} />;
+}
+
+type ChatroomProps = {
+  chatroomId: string;
+  authUser: User;
+};
+
+function Chatroom({ chatroomId, authUser }: ChatroomProps) {
   const { goBack } = useNavigation();
+  const getChatroom = useCallback(
+    () => chatApi.chatControllerGetChatroomById({ id: chatroomId }),
+    [chatroomId],
+  );
+  const { data: chatroom, isLoading: isChatroomLoading } = useData(getChatroom);
+  const getChatroomMember = useCallback(
+    () =>
+      chatApi.chatControllerGetChatroomMember({
+        chatroomId,
+        userId: authUser.id,
+      }),
+    [chatroomId, authUser.id],
+  );
+  const { data: chatroomMember, isLoading: isChatroomMemberLoading } =
+    useData(getChatroomMember);
 
   useEffect(() => {
     socket.on('exception', (wsError: WsException) => {
       // TODO Add error notification in case of an WebSocket exception
       console.log(wsError);
     });
-    if (chatroomId) {
-      socket.emit('joinChatroom', { chatroomId });
-    }
+    socket.emit('joinChatroom', { chatroomId });
 
     return () => {
       socket.off('exception');
-      if (chatroomId) {
-        socket.emit('leaveChatroom', { chatroomId });
-      }
+      socket.emit('leaveChatroom', { chatroomId });
     };
   }, [chatroomId]);
 
-  if (!chatroomId) {
-    return null;
+  if (isChatroomLoading || isChatroomMemberLoading) {
+    return (
+      <div className="chatroom">
+        <div className="chatroom-loading">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+
+  if (!chatroom) {
+    return <NotFoundPage />;
+  }
+
+  if (!chatroomMember) {
+    // TODO Redirect to the chatroom details page, where the user can join this chatroom
+    window.location.assign('/api#/chat/ChatController_createChatroomMember');
   }
 
   return (
     <div className="chatroom">
       <div className="chatroom-header">
         <Header icon={IconVariant.ARROW_BACK} onClick={goBack}>
-          {chatroomId}
+          {chatroom.name}
         </Header>
       </div>
       <div className="chatroom-messages">
@@ -48,5 +93,3 @@ function Chatroom() {
     </div>
   );
 }
-
-export default Chatroom;
