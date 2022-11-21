33aaf344b7f0ef68a210c9833c0887509573c0e8
Alvaro Ollero <aollero@gmail.com>
2022-11-21 22:28:09 +0200

    drafted solution of edit user page in avatar template page
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/pages/EditUserPage/EditUserPage.tsx
+++ webapp/src/pages/EditUserPage/EditUserPage.tsx
@@ -1,45 +1,28 @@
-import './EditUserPage.css';
-import {
-  EditUserForm,
-  Header,
-  IconVariant,
-  LargeAvatar,
-  Loading,
-} from '../../shared/components';
+import { AvatarPageTemplate, EditUserForm } from '../../shared/components';
 import { EDIT_AVATAR_URL, AVATAR_EP_URL } from '../../shared/urls';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../../shared/hooks/UseAuth';
-import { useNavigation } from '../../shared/hooks/UseNavigation';
 
 export default function EditUserPage() {
   const { username } = useParams();
-  const { authUser } = useAuth(username);
-  const { goBack } = useNavigation();
+  const { authUser, isLoading } = useAuth(username);
 
-  return authUser === null ? (
-    <div className="edit-user-page">
-      <div className="edit-user-loading">
-        <Loading />
-      </div>
-    </div>
-  ) : (
-    <div className="edit-user-page">
-      <Header icon={IconVariant.ARROW_BACK} onClick={goBack}>
-        edit profile
-      </Header>
-      <div className="edit-user-avatar">
-        <LargeAvatar
-          url={`${AVATAR_EP_URL}/${authUser.avatarId}`}
-          editUrl={EDIT_AVATAR_URL}
-          XCoordinate={authUser.avatarX}
-          YCoordinate={authUser.avatarY}
-        />
-      </div>
+  return (
+    <AvatarPageTemplate
+      isLoading={isLoading && !authUser}
+      title="edit profile"
+      avatarProps={{
+        url: `${AVATAR_EP_URL}/${authUser!.avatarId}`,
+        editUrl: EDIT_AVATAR_URL,
+        XCoordinate: authUser!.avatarX,
+        YCoordinate: authUser!.avatarY,
+      }}
+    >
       <EditUserForm
-        origUsername={authUser.username}
-        origFullName={authUser.fullName}
-        origEmail={authUser.email}
+        origUsername={authUser!.username}
+        origFullName={authUser!.fullName}
+        origEmail={authUser!.email}
       />
-    </div>
+    </AvatarPageTemplate>
   );
 }
