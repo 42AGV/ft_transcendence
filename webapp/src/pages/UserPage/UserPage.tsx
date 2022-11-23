b51f18fa415db49325a03e9d639e17cdd22ebd1e
Alvaro Ollero <aollero@gmail.com>
2022-11-23 02:28:11 +0200

    user page fitted
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/pages/UserPage/UserPage.tsx
+++ webapp/src/pages/UserPage/UserPage.tsx
@@ -1,24 +1,19 @@
 import './UserPage.css';
 import {
-  Header,
   IconVariant,
-  LargeAvatar,
-  Loading,
   Text,
   TextColor,
   TextVariant,
   TextWeight,
   ToggleSwitch,
-  Button,
   ButtonVariant,
+  AvatarPageTemplate,
 } from '../../shared/components';
 import { AVATAR_EP_URL } from '../../shared/urls';
 import { useData } from '../../shared/hooks/UseData';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { usersApi } from '../../shared/services/ApiService';
-import { useNavigation } from '../../shared/hooks/UseNavigation';
-import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import { useBlock } from '../../shared/hooks/UseBlock';
 
 export default function UserPage() {
@@ -28,76 +23,56 @@ export default function UserPage() {
     [username],
   );
   const { data: user, isLoading } = useData(getUserByUserName);
-  const { goBack } = useNavigation();
   const { blockRelation, unblockUser, blockUser } = useBlock(user);
 
-  if (isLoading) {
-    return (
-      <div className="user-page">
-        <div className="user-page-loading">
-          <Loading />
-        </div>
-      </div>
-    );
-  }
-
-  return user === null ? (
-    <NotFoundPage />
-  ) : (
+  return (
     <div className="user-page">
-      <Header
-        icon={IconVariant.ARROW_BACK}
-        onClick={goBack}
-        statusVariant="online"
+      <AvatarPageTemplate
+        isLoading={isLoading}
+        isNotFound={user === null}
+        title={user?.username ?? ''}
+        avatarProps={{
+          url: `${AVATAR_EP_URL}/${user?.avatarId ?? ''}`,
+          caption: 'level 4',
+          XCoordinate: user?.avatarX ?? 0,
+          YCoordinate: user?.avatarY ?? 0,
+        }}
+        button={
+          (blockRelation && {
+            disabled:
+              (blockRelation.amIBlocked || blockRelation.isUserBlocked) ??
+              false,
+            variant: ButtonVariant.SUBMIT,
+            iconVariant: IconVariant.SEND,
+            children: 'Send message',
+          }) ||
+          undefined
+        }
       >
-        {user.username}
-      </Header>
-      <div className="user-wrapper">
-        <LargeAvatar
-          url={`${AVATAR_EP_URL}/${user.avatarId}`}
-          caption="level 4"
-          XCoordinate={user.avatarX}
-          YCoordinate={user.avatarY}
-        />
-        <div className="user-text">
+        <>
           <Text
             variant={TextVariant.PARAGRAPH}
             color={TextColor.LIGHT}
             weight={TextWeight.MEDIUM}
           >
-            {user.fullName}
+            {user?.fullName ?? ''}
           </Text>
           <Text
             variant={TextVariant.PARAGRAPH}
             color={TextColor.LIGHT}
             weight={TextWeight.MEDIUM}
           >
-            {user.email}
+            {user?.email ?? ''}
           </Text>
-        </div>
-        <div className="user-block-status-toggle">
-          {blockRelation !== null && (
+          {blockRelation && (
             <ToggleSwitch
               label={blockRelation.isUserBlocked ? 'Unblock' : 'Block'}
               isToggled={blockRelation.isUserBlocked ?? false}
               onToggle={blockRelation.isUserBlocked ? unblockUser : blockUser}
             />
           )}
-        </div>
-      </div>
-      {blockRelation !== null && (
-        <div className="user-page-chat-button">
-          <Button
-            disabled={
-              (blockRelation.amIBlocked || blockRelation.isUserBlocked) ?? false
-            }
-            variant={ButtonVariant.SUBMIT}
-            iconVariant={IconVariant.SEND}
-          >
-            Send message
-          </Button>
-        </div>
-      )}
+        </>
+      </AvatarPageTemplate>
     </div>
   );
 }
