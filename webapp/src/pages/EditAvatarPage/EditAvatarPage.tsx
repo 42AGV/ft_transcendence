7c7775013185362dd8f8afa3e5586bdf0a485a39
gbudau <54190400+gbudau@users.noreply.github.com>
2022-12-02 02:42:02 +0100

    Fix lint

--- webapp/src/pages/EditAvatarPage/EditAvatarPage.tsx
+++ webapp/src/pages/EditAvatarPage/EditAvatarPage.tsx
@@ -18,7 +18,7 @@ import {
   EDITABLE_AVATAR_SCALE,
   EDITABLE_AVATAR_SCALE_REVERSE,
 } from '../../shared/components/Avatar/EditableAvatar';
-import {AvatarResponseDto } from '../../shared/generated';
+import { AvatarResponseDto } from '../../shared/generated';
 import { useNavigation } from '../../shared/hooks/UseNavigation';
 import { useNotificationContext } from '../../shared/context/NotificationContext';
 
