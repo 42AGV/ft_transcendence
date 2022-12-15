5f073d8265ed4affa04f00070e404839967f4ec7
Alvaro Ollero <aollero@gmail.com>
2022-12-15 14:10:35 +0200

    not the time and place for game queue page
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/pages/GameQueuePage/GameQueuePage.tsx
+++
@@ -1,9 +0,0 @@
-import './GameQueuePage.css';
-
-export default function GameQueuePage() {
-  return (
-    <div className="not-found-page">
-    <span className="loader"></span>
-    </div>
-  );
-}
