e44e689eef34ab9e4da996ff00da9645183bfc34
Alvaro Ollero <aollero@gmail.com>
2022-12-21 00:43:57 +0200

    addressed authorization module dependencies review
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- transcendence-app/src/authorization/authorization.module.ts
+++ transcendence-app/src/authorization/authorization.module.ts
@@ -1,26 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CaslAbilityFactory } from './casl-ability.factory';
 import { AuthorizationService } from './authorization.service';
-import { ChatroomMemberService } from '../chat/chatroom/chatroom-member/chatroom-member.service';
-import { ChatModule } from '../chat/chat.module';
 import { DbModule } from '../shared/db/db.module';
-import { LocalFileModule } from '../shared/local-file/local-file.module';
-import { AvatarModule } from '../shared/avatar/avatar.module';
-import { ChatService } from '../chat/chat.service';
 
 @Module({
-  providers: [
-    CaslAbilityFactory,
-    ChatroomMemberService,
-    AuthorizationService,
-    ChatService,
-  ],
-  imports: [
-    forwardRef(() => ChatModule),
-    DbModule,
-    LocalFileModule,
-    AvatarModule,
-  ],
+  providers: [CaslAbilityFactory, AuthorizationService],
+  imports: [DbModule],
   exports: [CaslAbilityFactory, AuthorizationService],
 })
 export class AuthorizationModule {}
