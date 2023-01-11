dff8602002c76b2db6ac4e49ca7019e16d5d2d59
gbudau <54190400+gbudau@users.noreply.github.com>
2023-01-11 12:35:11 +0000

    Fix TwoFactorAuthenticated guards

--- transcendence-app/src/shared/guards/two-factor-authenticated.guard.ts
+++ transcendence-app/src/shared/guards/two-factor-authenticated.guard.ts
@@ -1,13 +1,17 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthenticatedGuard } from './authenticated.guard';
+import { User } from '../../user/infrastructure/db/user.entity';
 
 @Injectable()
 export class TwoFactorAuthenticatedGuard extends AuthenticatedGuard {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    return (
-      (await super.canActivate(context)) &&
-      request.session.isTwoFactorAuthenticated
-    );
+    const user: User = request.user;
+    const isAuthenticated = await super.canActivate(context);
+
+    if (!user.isTwoFactorAuthenticationEnabled) {
+      return isAuthenticated;
+    }
+    return isAuthenticated && request.session.isTwoFactorAuthenticated;
   }
 }
--- transcendence-app/src/shared/guards/ws-two-factor-authenticated.guard.ts
+++ transcendence-app/src/shared/guards/ws-two-factor-authenticated.guard.ts
@@ -1,14 +1,19 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { WsAuthenticatedGuard } from './ws-authenticated.guard';
+import { User } from '../../user/infrastructure/db/user.entity';
 
 @Injectable()
 export class WsTwoFactorAuthenticatedGuard extends WsAuthenticatedGuard {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const client = context.switchToWs().getClient();
     const request = client.request;
-    return (
-      (await super.canActivate(context)) &&
-      request.session.isTwoFactorAuthenticated
-    );
+    const user: User = request.user;
+    const isAuthenticated = await super.canActivate(context);
+
+    if (!user.isTwoFactorAuthenticationEnabled) {
+      return isAuthenticated;
+    }
+
+    return isAuthenticated && request.session.isTwoFactorAuthenticated;
   }
 }
