9aeea1aea59ba9231bc66e9a58ee5cb851c1dc66
Alvaro Ollero <aollero@gmail.com>
2023-01-26 11:51:57 +0200

    better alert
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/shared/components/ConfirmAlert/CustomConfirmAlert.tsx
+++ webapp/src/shared/components/ConfirmAlert/CustomConfirmAlert.tsx
@@ -16,34 +16,36 @@ export default function CustomConfirmAlert({
   confirmAlert({
     customUI: ({ onClose }) => {
       return (
-        <div className="popup-overlay">
+        <div className="react-confirm-alert-body">
           <Text variant={TextVariant.SUBHEADING}>{title ?? ''}</Text>
           <Text variant={TextVariant.PARAGRAPH}>{message ?? ''}</Text>
-          {buttons.map((prop, i) => {
-            const { onClick, ...restProps } = prop;
-            if (onClick) {
-              if (i !== 0) {
-                const newClick: React.MouseEventHandler<HTMLButtonElement> = (
-                  e,
-                ) => {
-                  onClick(e);
-                  onClose();
-                };
-                return (
-                  <Button {...{ ...restProps, onClick: newClick }} key={i} />
-                );
-              } else {
-                const newClick: React.MouseEventHandler<
-                  HTMLButtonElement
-                > = () => {
-                  onClose();
-                };
-                return (
-                  <Button {...{ ...restProps, onClick: newClick }} key={i} />
-                );
+          <div className="react-confirm-alert-button-group">
+            {buttons.map((prop, i) => {
+              const { onClick, ...restProps } = prop;
+              if (onClick) {
+                if (i === 0) {
+                  const newClick: React.MouseEventHandler<HTMLButtonElement> = (
+                    e,
+                  ) => {
+                    onClick(e);
+                    onClose();
+                  };
+                  return (
+                    <Button {...{ ...restProps, onClick: newClick }} key={i} />
+                  );
+                } else {
+                  const newClick: React.MouseEventHandler<
+                    HTMLButtonElement
+                  > = () => {
+                    onClose();
+                  };
+                  return (
+                    <Button {...{ ...restProps, onClick: newClick }} key={i} />
+                  );
+                }
               }
-            }
-          })}
+            })}
+          </div>
         </div>
       );
     },
