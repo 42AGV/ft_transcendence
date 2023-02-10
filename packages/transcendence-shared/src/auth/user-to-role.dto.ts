b293ab86b8d8572c68e69b27548c2b7fd7e4b3dd
Alvaro Ollero <aollero@gmail.com>
2023-02-10 22:22:36 +0200

    draft transcendence-shared, building properly in mainstream npm run jobs
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- packages/transcendence-shared/src/auth/user-to-role.dto.ts
+++ packages/transcendence-shared/src/auth/user-to-role.dto.ts
@@ -1,9 +1,13 @@
+import { IsEnum, IsOptional, IsUUID } from 'class-validator';
 import { Role } from '../shared/enums/role.enum';
 
 export class UserToRoleDto {
+  @IsUUID()
   id!: string;
 
+  @IsEnum(Role)
   role!: Role;
 
+  @IsOptional()
   username?: string;
 }
---
+++ packages/transcendence-shared/src/index.ts
@@ -0,0 +1,4 @@
+export * from './auth/user-to-role.dto';
+export * from './game/queue.dto';
+export * from './shared/enums/role.enum';
+export * from './constants';
