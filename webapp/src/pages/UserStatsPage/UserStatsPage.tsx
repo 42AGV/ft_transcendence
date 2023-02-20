d61990a5a546190522d2e4a22976245e14f9f6e2
Alvaro Ollero <aollero@gmail.com>
2023-02-21 00:00:32 +0200

    non working but bootstrapping
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/pages/UserStatsPage/UserStatsPage.tsx
+++ webapp/src/pages/UserStatsPage/UserStatsPage.tsx
@@ -1,10 +1,21 @@
 import './UserStatsPage.css';
-import { Loading } from '../../shared/components';
+import { Line } from 'react-chartjs-2';
 
 export default function UserStatsPage() {
   return (
-    <div className="loading-page">
-      <Loading />
+    <div className="stats-page">
+      <Line
+        data={{
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [
+            {
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              borderWidth: 1,
+            },
+          ],
+        }}
+      />
     </div>
   );
 }
