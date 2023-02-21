707b97fd6dc228e8a9c543dc03fd22e3d6870724
Alvaro Ollero <aollero@gmail.com>
2023-02-21 11:46:33 +0200

    cartesian date chart for level implemented
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/pages/UserStatsPage/UserStatsPage.tsx
+++ webapp/src/pages/UserStatsPage/UserStatsPage.tsx
@@ -3,8 +3,14 @@ import { Line } from 'react-chartjs-2';
 import { gameApi } from '../../shared/services/ApiService';
 import { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import { GameControllerGetUserLevelHistoryModeEnum } from '../../shared/generated';
+import {
+  GameControllerGetUserLevelHistoryModeEnum,
+  UserLevelWithTimestamp,
+} from '../../shared/generated';
 import { useData } from '../../shared/hooks/UseData';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import { LoadingPage } from '../index';
+import 'chartjs-adapter-date-fns';
 
 export default function UserStatsPage() {
   const { username } = useParams()!;
@@ -13,21 +19,23 @@ export default function UserStatsPage() {
       gameApi.gameControllerGetUserLevelHistory({
         username: username!,
         mode: GameControllerGetUserLevelHistoryModeEnum.Classic,
-        limit: 100000,
+        limit: 20,
         offset: 0,
       }),
-    [],
+    [username],
   );
-  const { data, isLoading } = useData(getData);
+  const { data, isLoading } = useData<UserLevelWithTimestamp[]>(getData);
+  if (isLoading) return <LoadingPage />;
+  if (!data) return <NotFoundPage />;
   return (
     <div className="stats-page">
       <Line
         data={{
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: data.map((item) => item.timestamp),
           datasets: [
             {
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
+              label: 'level over time',
+              data: data.map((item) => item.level),
               borderWidth: 1,
               stepped: true,
             },
@@ -35,8 +43,14 @@ export default function UserStatsPage() {
         }}
         options={{
           scales: {
-            y: {
-              beginAtZero: true,
+            x: {
+              type: 'time',
+              time: {
+                unit: 'day',
+                displayFormats: {
+                  quarter: 'MMM YYYY',
+                },
+              },
             },
           },
         }}
