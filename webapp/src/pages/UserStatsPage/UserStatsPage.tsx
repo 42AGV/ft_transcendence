e988e285f95d76931d30cdbb824415d8782f71fa
Alvaro Ollero <aollero@gmail.com>
2023-02-21 10:32:24 +0200

    draft of data retrieve
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/pages/UserStatsPage/UserStatsPage.tsx
+++ webapp/src/pages/UserStatsPage/UserStatsPage.tsx
@@ -1,7 +1,24 @@
 import './UserStatsPage.css';
 import { Line } from 'react-chartjs-2';
+import { gameApi } from '../../shared/services/ApiService';
+import { useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { GameControllerGetUserLevelHistoryModeEnum } from '../../shared/generated';
+import { useData } from '../../shared/hooks/UseData';
 
 export default function UserStatsPage() {
+  const { username } = useParams()!;
+  const getData = useCallback(
+    () =>
+      gameApi.gameControllerGetUserLevelHistory({
+        username: username!,
+        mode: GameControllerGetUserLevelHistoryModeEnum.Classic,
+        limit: 100000,
+        offset: 0,
+      }),
+    [],
+  );
+  const { data, isLoading } = useData(getData);
   return (
     <div className="stats-page">
       <Line
