bfd484c340789b31bd82790c758b4cc2b1525d03
Alvaro Ollero <aollero@gmail.com>
2023-03-03 02:59:13 +0200

    draft, doughnut doesn't work
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- webapp/src/pages/UserStatsPage/UserStatsPage.tsx
+++ webapp/src/pages/UserStatsPage/UserStatsPage.tsx
@@ -4,11 +4,10 @@ import { gameApi, usersApi } from '../../shared/services/ApiService';
 import { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import {
-  UserLevelWithTimestamp,
   GameControllerGetUserLevelHistoryModeEnum,
   GameControllerGetUserStatsModeEnum,
-  UserLevelWithTimestampGameModeEnum,
   GameStats,
+  UserLevelWithTimestamp,
 } from '../../shared/generated';
 import { useData } from '../../shared/hooks/UseData';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
@@ -17,44 +16,104 @@ import 'chartjs-adapter-date-fns';
 import { Header, IconVariant } from '../../shared/components';
 import { useNavigation } from '../../shared/hooks/UseNavigation';
 
+type FiveArray<T> = [T, T, T, T, T];
+type proArray = [
+  UserLevelWithTimestamp[],
+  GameControllerGetUserLevelHistoryModeEnum,
+];
 export default function UserStatsPage() {
   const { username } = useParams()!;
   const { goBack } = useNavigation();
-  const getLevelHistory = useCallback(
-    () =>
-      gameApi.gameControllerGetUserLevelHistory({
-        username: username!,
-        mode: GameControllerGetUserLevelHistoryModeEnum.Classic,
-      }),
-    [username],
-  );
-  const { data: levels, isLoading: areLevelsLoading } =
-    useData<UserLevelWithTimestamp[]>(getLevelHistory);
-  const getStats = useCallback(
-    () =>
-      gameApi.gameControllerGetUserStats({
-        username: username!,
-        mode: GameControllerGetUserStatsModeEnum.Classic,
-      }),
-    [username],
-  );
-  const { data: stats, isLoading: areStatsLoading } =
-    useData<GameStats>(getStats);
   const getUserByUserName = useCallback(
     () => usersApi.userControllerGetUserByUserName({ userName: username! }),
     [username],
   );
   const { data: user, isLoading: isUserLoading } = useData(getUserByUserName);
+  const getLevelHistory = useCallback(async (): Promise<
+    FiveArray<UserLevelWithTimestamp[]>
+  > => {
+    try {
+      if (!user) {
+        return Promise.reject('Unresolved');
+      }
+      let pclassic: proArray = [
+        await gameApi.gameControllerGetUserLevelHistory({
+          username: username!,
+          mode: GameControllerGetUserLevelHistoryModeEnum.Classic,
+        }),
+        GameControllerGetUserLevelHistoryModeEnum.Classic,
+      ];
+      let pmystery: proArray = [
+        await gameApi.gameControllerGetUserLevelHistory({
+          username: username!,
+          mode: GameControllerGetUserLevelHistoryModeEnum.MysteryZone,
+        }),
+        GameControllerGetUserLevelHistoryModeEnum.MysteryZone,
+      ];
+      let pshort: proArray = [
+        await gameApi.gameControllerGetUserLevelHistory({
+          username: username!,
+          mode: GameControllerGetUserLevelHistoryModeEnum.ShortPaddle,
+        }),
+        GameControllerGetUserLevelHistoryModeEnum.ShortPaddle,
+      ];
+      let ptrain: proArray = [
+        await gameApi.gameControllerGetUserLevelHistory({
+          username: username!,
+          mode: GameControllerGetUserLevelHistoryModeEnum.Training,
+        }),
+        GameControllerGetUserLevelHistoryModeEnum.Training,
+      ];
+      let punknown: proArray = [
+        await gameApi.gameControllerGetUserLevelHistory({
+          username: username!,
+          mode: GameControllerGetUserLevelHistoryModeEnum.Unknown,
+        }),
+        GameControllerGetUserLevelHistoryModeEnum.Unknown,
+      ];
+      [pclassic, pmystery, pshort, ptrain, punknown].map((levels) => {
+        return levels[0].unshift({
+          username: user.username,
+          timestamp: user.createdAt,
+          gameMode: levels[1],
+          level: 1,
+          gameId: '', // maybe we should allow null in gameId here,
+        });
+      });
+      return [pclassic[0], pmystery[0], pshort[0], ptrain[0], punknown[0]];
+    } catch (e) {
+      return Promise.reject('Unresolved');
+    }
+  }, [user, username]);
+  const { data: levels, isLoading: areLevelsLoading } = useData<
+    FiveArray<UserLevelWithTimestamp[]>
+  >(
+    getLevelHistory,
+    useCallback(() => {}, []),
+  );
+  const getStats = useCallback(
+    () =>
+      Promise.all(
+        [
+          GameControllerGetUserStatsModeEnum.Classic,
+          GameControllerGetUserStatsModeEnum.ShortPaddle,
+          GameControllerGetUserStatsModeEnum.MysteryZone,
+          GameControllerGetUserStatsModeEnum.Training,
+          GameControllerGetUserStatsModeEnum.Unknown,
+        ].map((mode) => {
+          return gameApi.gameControllerGetUserStats({
+            username: username!,
+            mode,
+          });
+        }),
+      ),
+    [username],
+  );
+  const { data: stats, isLoading: areStatsLoading } =
+    useData<GameStats[]>(getStats);
   if (areLevelsLoading || isUserLoading || areStatsLoading)
     return <LoadingPage />;
   if (!levels || !user || !stats) return <NotFoundPage />;
-  levels.unshift({
-    username: user.username,
-    timestamp: user.createdAt,
-    gameMode: UserLevelWithTimestampGameModeEnum.Classic,
-    level: 1,
-    gameId: '', // maybe we should allow null in gameId here,
-  });
   return (
     <div className="stats-page">
       <Header icon={IconVariant.ARROW_BACK} onClick={goBack}>
@@ -64,15 +123,19 @@ export default function UserStatsPage() {
         <div className="line-graph-wrapper">
           <Line
             data={{
-              labels: levels.map((item) => item.timestamp),
-              datasets: [
-                {
-                  label: 'level',
-                  data: levels.map((item) => item.level),
+              datasets: levels.map((level) => {
+                return {
+                  label: level[0].gameMode,
+                  data: level.map((item) => {
+                    return {
+                      x: item.timestamp,
+                      y: item.level,
+                    };
+                  }),
                   borderWidth: 1,
                   stepped: true,
-                },
-              ],
+                };
+              }),
             }}
             options={{
               plugins: {
@@ -102,7 +165,11 @@ export default function UserStatsPage() {
               labels: ['win', 'loss', 'draw'],
               datasets: [
                 {
-                  data: [stats.wins, stats.losses, stats.draws],
+                  data: stats.map((stat) => [
+                    stat.wins,
+                    stat.losses,
+                    stat.draws,
+                  ]),
                 },
               ],
             }}
