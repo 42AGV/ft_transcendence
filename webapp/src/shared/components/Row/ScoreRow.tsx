71a4dfbaf028ed303232054a8036f9afa4294f5c
fizcamposancos <56964234+fizcamposancos@users.noreply.github.com>
2023-03-10 00:01:13 +0100

    #582/Fix ScoreRow list appearance (#583)
    * some fixes, not everything yet
    
    * appearance corrections
    
    * suggestions accepted

--- webapp/src/shared/components/Row/ScoreRow.tsx
+++ webapp/src/shared/components/Row/ScoreRow.tsx
@@ -1,7 +1,7 @@
 import './ScoreRow.css';
 import { TextColor, TextVariant, TextWeight } from '../Text/Text';
 import { default as Text } from '../Text/Text';
-import { MediumAvatar } from '../Avatar/Avatar';
+import { MediumAvatar, SmallAvatar } from '../Avatar/Avatar';
 import { Link } from 'react-router-dom';
 import { ScoreProps } from '../Score/Score';
 
@@ -27,69 +27,91 @@ export default function ScoreRow({
     <>
       {scoreProps.playerOneAvatar && (
         <div className="row-avatar-left">
-          <MediumAvatar {...scoreProps.playerOneAvatar} />
+          <div className="row-avatar-left-medium">
+            <MediumAvatar {...scoreProps.playerOneAvatar} />
+          </div>
+          <div className="row-avatar-left-small">
+            <SmallAvatar {...scoreProps.playerOneAvatar} />
+          </div>
         </div>
       )}
       <div className="scorerow_text_wrapper">
         <div className="scorerow_text_upper_wrapper">
-          <Text
-            variant={TextVariant.HEADING}
-            color={TextColor.LIGHT}
-            weight={TextWeight.BOLD}
-          >
-            {scoreProps.playerOneUserName}
-          </Text>
-          <Text
-            variant={TextVariant.HEADING}
-            color={TextColor.LIGHT}
-            weight={TextWeight.BOLD}
-          >
-            {scoreProps.playerOneScore === undefined ||
-            scoreProps.playerTwoScore === undefined
-              ? ' - '
-              : `${scoreProps.playerOneScore!.toString()} - ${scoreProps.playerTwoScore!.toString()} `}
-          </Text>
-          <Text
-            variant={TextVariant.HEADING}
-            color={TextColor.LIGHT}
-            weight={TextWeight.BOLD}
-          >
-            {scoreProps.playerTwoUserName}
-          </Text>
+          <div className="scorerow_text_upper_wrapper_p1">
+            <Text
+              variant={TextVariant.SUBHEADING}
+              color={TextColor.LIGHT}
+              weight={TextWeight.REGULAR}
+            >
+              {scoreProps.playerOneUserName}
+            </Text>
+          </div>
+          <div className="scorerow_text_upper_wrapper_score">
+            <Text
+              variant={TextVariant.HEADING}
+              color={TextColor.LIGHT}
+              weight={TextWeight.BOLD}
+            >
+              {scoreProps.playerOneScore === undefined ||
+              scoreProps.playerTwoScore === undefined
+                ? ' - '
+                : `${scoreProps.playerOneScore!.toString()} - ${scoreProps.playerTwoScore!.toString()} `}
+            </Text>
+          </div>
+          <div className="scorerow_text_upper_wrapper_p2">
+            <Text
+              variant={TextVariant.SUBHEADING}
+              color={TextColor.LIGHT}
+              weight={TextWeight.REGULAR}
+            >
+              {scoreProps.playerTwoUserName}
+            </Text>
+          </div>
         </div>
         <div className="scorerow_text_lower_wrapper">
-          {gameMode && (
-            <Text
-              variant={TextVariant.PARAGRAPH}
-              color={TextColor.LIGHT}
-              weight={TextWeight.REGULAR}
-            >
-              {`Mode: ${gameMode} `}
-            </Text>
-          )}
-          {gameDuration && (
-            <Text
-              variant={TextVariant.PARAGRAPH}
-              color={TextColor.LIGHT}
-              weight={TextWeight.REGULAR}
-            >
-              {`Duration: ${gameDuration}sec. `}
-            </Text>
-          )}
-          {date && (
-            <Text
-              variant={TextVariant.PARAGRAPH}
-              color={TextColor.LIGHT}
-              weight={TextWeight.REGULAR}
-            >
-              {`Date: ${date.toLocaleString('en-GB')}`}
-            </Text>
-          )}
+          <div className="scorerow_text_lower_wrapper_mode">
+            {gameMode && (
+              <Text
+                variant={TextVariant.PARAGRAPH}
+                color={TextColor.LIGHT}
+                weight={TextWeight.REGULAR}
+              >
+                {`Mode: ${gameMode} `}
+              </Text>
+            )}
+          </div>
+          <div className="scorerow_text_lower_wrapper_duration">
+            {gameDuration && (
+              <Text
+                variant={TextVariant.PARAGRAPH}
+                color={TextColor.LIGHT}
+                weight={TextWeight.REGULAR}
+              >
+                {`Duration: ${gameDuration}sec. `}
+              </Text>
+            )}
+          </div>
+          <div className="scorerow_text_lower_wrapper_date">
+            {date && (
+              <Text
+                variant={TextVariant.PARAGRAPH}
+                color={TextColor.LIGHT}
+                weight={TextWeight.REGULAR}
+              >
+                {`Date: ${date.toLocaleString('en-GB')}`}
+              </Text>
+            )}
+          </div>
         </div>
       </div>
       {scoreProps.playerTwoAvatar && (
         <div className="row-avatar-right">
-          <MediumAvatar {...scoreProps.playerTwoAvatar} />
+          <div className="row-avatar-right-medium">
+            <MediumAvatar {...scoreProps.playerTwoAvatar} />
+          </div>
+          <div className="row-avatar-right-small">
+            <SmallAvatar {...scoreProps.playerTwoAvatar} />
+          </div>
         </div>
       )}
     </>
