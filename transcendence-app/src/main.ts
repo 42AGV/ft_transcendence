db720b40b103c96276e721b0228ffeeb2895acd6
Alvaro Ollero <aollero@gmail.com>
2023-03-21 14:26:45 +0200

    fix logger
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- transcendence-app/src/main.ts
+++ transcendence-app/src/main.ts
@@ -4,9 +4,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { setupApp } from './setup-app';
+import { TranscendenceLogger } from './shared/utils';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: new TranscendenceLogger(),
+  });
   app.enable('trust proxy');
   app.setGlobalPrefix('api');
   app.enableVersioning({
@@ -23,4 +26,5 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
+
 bootstrap();
--- transcendence-app/src/shared/utils.ts
+++ transcendence-app/src/shared/utils.ts
@@ -1,3 +1,5 @@
+import { ConsoleLogger } from '@nestjs/common';
+
 export function loadEsmModule<T>(modulePath: string): Promise<T> {
   return Function(
     'modulePath',
@@ -11,4 +13,13 @@ export function removeDoubleQuotes(string: string) {
 
 export function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export class TranscendenceLogger extends ConsoleLogger {
+  error(message: any, stack?: string, context?: string) {
+    if (context === 'ExpressAdapter' && message === 'Premature close') {
+      return;
+    }
+    super.error(message, stack, context);
+  }
 }
