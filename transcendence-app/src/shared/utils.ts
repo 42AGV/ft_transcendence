ab70956087a76053d7c1087ade46dadfd8b847ab
Damrod <aollero@gmail.com>
2023-03-21 18:28:06 +0200

    #386 fix express adapter premature close error (#605)
    * fix logger
    
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>
    
    * addressed review
    
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>
    
    ---------
    
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- transcendence-app/src/shared/utils.ts
+++ transcendence-app/src/shared/utils.ts
@@ -1,3 +1,5 @@
+import { ConsoleLogger } from '@nestjs/common';
+
 export function loadEsmModule<T>(modulePath: string): Promise<T> {
   return Function(
     'modulePath',
@@ -11,4 +13,23 @@ export function removeDoubleQuotes(string: string) {
 
 export function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export class TranscendenceLogger extends ConsoleLogger {
+  error(message: any, stack?: string, context?: string) {
+    /**
+     * When using StreamableFile to stream user avatars efficiently, it's
+     * possible for clients to prematurely close the connection while the
+     * server is streaming the file. This can trigger a 'Premature close'
+     * error in Node.js. However, this behavior is expected and does not
+     * indicate a server error. To minimize false error noise and improve
+     * overall system reliability, we've disabled this error in our Console
+     * logger class.
+     *
+     */
+    if (context === 'ExpressAdapter' && message === 'Premature close') {
+      return;
+    }
+    super.error(message, stack, context);
+  }
 }
