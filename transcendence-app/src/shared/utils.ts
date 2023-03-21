eab9084c8e7b5e8b0da0834f18bf47bc6538b91e
Alvaro Ollero <aollero@gmail.com>
2023-03-21 18:06:07 +0200

    addressed review
    Signed-off-by: Alvaro Ollero <aollero@gmail.com>

--- transcendence-app/src/shared/utils.ts
+++ transcendence-app/src/shared/utils.ts
@@ -17,6 +17,16 @@ export function capitalizeFirstLetter(string: string) {
 
 export class TranscendenceLogger extends ConsoleLogger {
   error(message: any, stack?: string, context?: string) {
+    /**
+     * When using StreamableFile to stream user avatars efficiently, it's
+     * possible for clients to prematurely close the connection while the
+     * server is streaming the file. This can trigger a 'Premature close'
+     * error in Node.js. However, this behavior is expected and does not
+     * indicate a server error. To minimize false error noise and improve
+     * overall system reliability, we've disabled this error in our Console
+     * logger class.
+     *
+     */
     if (context === 'ExpressAdapter' && message === 'Premature close') {
       return;
     }
